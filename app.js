f523d121d867e9489347b68e96a76333e2a9698b
Sahinur Islam <infosahinur@gmail.com>
2021-02-19 01:58:43 +0600

    new

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-        const searchBtn = document.getElementById('searchItemBtn');
+    const searchBtn = document.getElementById('searchItemBtn');
           // API 
-          const foodItemShow = () => {
+          const foodItemShow = async () => {
             const searchInput = document.getElementById('searchItem').value;
-            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
-            .then(res => res.json())
-            .then(data => foodShow(data))
-            .catch(erro => console.log(erro))
-          }
+            const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
+            try{
+                const res = await fetch(url);
+                const data = await res.json();
+                foodShow(data);
+            }
+            catch(error){
+                displayError('Sorry, try again!!!')
+            }
+          };
 
           const foodShow = (food) => {
             const foodArr = food.meals;
@@ -21,23 +26,23 @@
                     </div>
                 `;
                 rowDiv.innerHTML= singleFood;
-
-                document.getElementById('foodShoww').appendChild(rowDiv);
-
-                
+                document.getElementById('foodShoww').appendChild(rowDiv); 
             });
           }
 
-          const foodDetailsData = id => {
-              fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-              .then(res => res.json())
-              .then(data => foodDetailsDisplay(data.meals[0]))
-              .catch(error => console.log(error))
-          }
+          const foodDetailsData = async(id) => {
+              const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+              try{
+                const res = await fetch(url);
+                const data = await res.json();
+                foodDetailsDisplay(data.meals[0]);
+              }
+              catch(error){
+                  displayError('Sorry, try again!!!')
+              }
+          };
            
           const foodDetailsDisplay = meal => {
-              console.log(meal);
-            // const popUPDiv = document.createElement('div')
             const singleDetails = `
                         <img src="${meal.strMealThumb}">
                         <h6>Name: ${meal.strMeal}</h6>
@@ -47,6 +52,10 @@
                         <p>Food ingredients: ${meal.strIngredient1}, ${meal.strIngredient2}, ${meal.strIngredient3}, ${meal.strIngredient4}, ${meal.strIngredient4}, ${meal.strIngredient6}, ${meal.strIngredient1}</p>
                         
             `;
-            // popUPDiv.innerHTML = singleDetails;
             document.getElementById('details-food-contan').innerHTML = singleDetails;
+          }
+
+          const displayError = error => {
+              alert(error)
+            //   document.getElementById('error-massage').innerText = error;
           }
